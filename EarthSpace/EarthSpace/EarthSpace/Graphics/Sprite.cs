3e96fd25138222813ed4547747875ffd70998292
Nathaniel Nelson <lostcodestudios@gmail.com>
2013-12-10 14:21:42 -0700

    Test commit as Lost Code Studios.

---
+++ EarthSpace/EarthSpace/EarthSpace/Graphics/Sprite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EarthSpace.Graphics
+{
+    /// <summary>
+    /// A sprite.
+    /// </summary>
+    public class Sprite : IDrawable
+    {
+        public void Show()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+
+        public void Hide()
+        {
+        }
+    }
+}
