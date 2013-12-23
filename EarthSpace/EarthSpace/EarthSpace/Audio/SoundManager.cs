3259a074784ed0e951b5ed74d71e157d6df4cc54
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-12-23 14:50:06 -0700

    SoundManager work

--- EarthSpace/EarthSpace/EarthSpace/Audio/SoundManager.cs
+++ EarthSpace/EarthSpace/EarthSpace/Audio/SoundManager.cs
@@ -6,11 +6,17 @@ using System.Text;
 
 namespace EarthSpace.Audio
 {
+    /// <summary>
+    /// Manages the game's sound effects.
+    /// </summary>
     public static class SoundManager
     {
         static Dictionary<string, SoundEffect> sounds = new Dictionary<string, SoundEffect>();
         static float volume;
 
+        /// <summary>
+        /// The volume of the game's sound.
+        /// </summary>
         public static float Volume
         {
             get { return volume; }
@@ -23,17 +29,36 @@ namespace EarthSpace.Audio
             }
         }
 
-        public static void AddSong(string key, SoundEffect sound)
+        /// <summary>
+        /// Adds a sound effect to the manager.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="sound"></param>
+        public static void AddSound(string key, SoundEffect sound)
         {
             sounds.Add(key, sound);
         }
 
+        /// <summary>
+        /// Plays a sound effect.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="volume"></param>
+        /// <param name="pitch"></param>
+        /// <param name="pan"></param>
         public static void PlaySound(string key, float volume, float pitch, float pan)
         {
             sounds[key].Play(Volume * volume, pitch, pan);
         }
 
-        public static 
+        /// <summary>
+        /// Plays a sound effect.
+        /// </summary>
+        /// <param name="key"></param>
+        public static void PlaySound(string key)
+        {
+            PlaySound(key, 1f, 0f, 0f);
+        }
         
     }
 }
